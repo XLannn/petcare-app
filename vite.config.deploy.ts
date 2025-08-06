06cd6677d929da7e68274476d69344151fc864d8
XLannn <alanbijialex@gmail.com>
2025-08-06 22:07:05 +0530

    fix: Configure single file build for GitHub Pages routing

--- vite.config.deploy.ts
+++ vite.config.deploy.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/petcare-app/",
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
